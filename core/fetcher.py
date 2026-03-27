b7428f9f114c6b5e82a5e14cabd3e3f882f4c06e
RIFKI-NQ <rifqinf950@gmail.com>
2026-03-27 21:39:30 +0700

    feat(normalizer): add data normalizer for anilist response

--- core/fetcher.py
+++ core/fetcher.py
@@ -1,21 +1,20 @@
 import requests
 from abc import ABC, abstractmethod
-from typing import Literal, get_args, Any
+from typing import Any
+from core.models.anime_model import DATA_SOURCES, VALID_DATA_SOURCES
 from core.exceptions import InvalidDataSource
 
 class FetchData(ABC):
-    DATA_SOURCES = Literal["anilist"]
-    VALID_DATA_SOURCES = get_args(DATA_SOURCES)
     
     @staticmethod
     def create_fetcher(data_source: DATA_SOURCES) -> FetchData:
         if data_source == "anilist":
             return FetchAnilist()
         else:
-            raise InvalidDataSource(f"Invalid data source provided ({data_source}), expected ({FetchData.VALID_DATA_SOURCES})")
+            raise InvalidDataSource(f"Invalid data source provided ({data_source}), expected ({VALID_DATA_SOURCES})")
         
     @abstractmethod
-    def fetch_data(self, anime_title: str) -> dict[str, Any]:
+    def fetch_data(self, anime_title: str) -> dict[Any, Any]:
         pass
     
     def _request(self, url: str, query: str, variables: dict[str, str]) -> requests.Response:
@@ -39,6 +38,6 @@ class FetchAnilist(FetchData):
     }
     """
     
-    def fetch_data(self, anime_title: str) -> dict[str, Any]:
+    def fetch_data(self, anime_title: str) -> dict[Any, Any]:
         data = self._request(url=self.BASE_URL, query=self.QUERY, variables={"search": anime_title})
         return data.json()
---
+++ core/normalizer.py
@@ -0,0 +1,21 @@
+from core.fetcher import FetchData
+from core.models.anime_model import AnimeDataModel, DATA_SOURCES, VALID_DATA_SOURCES
+from core.exceptions import InvalidDataSource
+
+class ResponseNormalizer:
+    def __init__(self, fetcher: type[FetchData]) -> None:
+        self.anilist_fetcher = fetcher.create_fetcher("anilist")
+        
+    def get_anime_data(self, source: DATA_SOURCES, anime_title: str):
+        if source == "anilist":
+            return self._get_anilist_data(anime_title)
+        else:
+            raise InvalidDataSource(f"Invalid data source provided ({source}), expected ({VALID_DATA_SOURCES})")
+    
+    def _get_anilist_data(self, anime_title: str):
+        data = self.anilist_fetcher.fetch_data(anime_title)["data"]["Media"]
+        return AnimeDataModel(
+            source="anilist", id=data["id"],
+            english_title=data["title"]["english"], romaji_title=data["title"]["romaji"],
+            average_score=data["averageScore"], episodes=data["episodes"], genres=data["genres"]
+        )
