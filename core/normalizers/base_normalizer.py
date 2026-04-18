477c2c9e4ce79e828bdf94b866cfe5f1e22bd8d4
Rifki-NQ <rifqinf950@gmail.com>
2026-04-18 23:01:21 +0700

    refactor(normalizer): split normalizers and convert to abstract classes

---
+++ core/normalizers/base_normalizer.py
@@ -0,0 +1,16 @@
+from abc import ABC, abstractmethod
+from core.models.anime_model import AnimeDataModel
+
+class BaseNormalizer(ABC):
+    @abstractmethod
+    def get_anime_by_title(self, anime_title: str, entry_number: int  | None = None) -> AnimeDataModel:
+        pass
+    
+    @abstractmethod
+    def get_anime_by_id(self, anime_id: int, ) -> AnimeDataModel:
+        pass
+    
+    @abstractmethod
+    def get_all_anime_by_title(self, anime_title: str,
+                               max_entry: int | None = None) -> list[AnimeDataModel]:
+        pass
