372753f19d7a01e9d5dba2effb981af0379b4dd2
Rifki-NQ <rifqinf950@gmail.com>
2026-04-19 14:50:18 +0700

    feat(protocols): add NormalizerProtocol

--- core/models/protocols.py
+++ core/models/protocols.py
@@ -1,8 +1,21 @@
 from typing import Protocol, Any
+from core.models.anime_model import AnimeDataModel
 
 class FetchersProtocol(Protocol):
     def fetch_data_by_title(self, anime_title: str) -> list[dict[str, Any]]:
         ...
 
     def fetch_data_by_id(self, anime_id: int) -> dict[str, Any]:
+        ...
+        
+
+class NormalizerProtocol(Protocol):
+    def get_anime_by_title(self, anime_title: str, entry_number: int  = 0) -> AnimeDataModel:
+        ...
+    
+    def get_anime_by_id(self, anime_id: int, ) -> AnimeDataModel:
+        ...
+    
+    def get_all_anime_by_title(self, anime_title: str,
+                               max_entry: int | None = None) -> list[AnimeDataModel]:
         ...
