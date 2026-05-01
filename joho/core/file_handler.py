9bebf18b23ccbd746ee844652dc806dcd4e27629
Rifki-NQ <rifqinf950@gmail.com>
2026-05-02 05:55:47 +0700

    refactor(file_handler): remove pandas dependency and use built-in csv module instead

--- joho/core/file_handler.py
+++ joho/core/file_handler.py
@@ -1,4 +1,4 @@
-import pandas as pd
+import csv
 from pathlib import Path
 from dataclasses import fields, asdict
 from joho.core.models.anime_model import AnimeDataModel
@@ -8,25 +8,14 @@ class DataIO:
     def __init__(self, filepath: Path) -> None:
         self.filepath = filepath
 
-    def save_data(self, new_data: AnimeDataModel, overwrite: bool = False) -> None:
-        df_new_data = pd.DataFrame([asdict(new_data)])
-        if overwrite:
-            df_new_data.to_csv(self.filepath, index=False)
-            return
-        df_previous_data = self._read_file()
-        df_merged_data = pd.concat([df_previous_data, df_new_data], ignore_index=True)
-        df_merged_data.to_csv(self.filepath, index=False)
+    def save_data(self, data: AnimeDataModel, overwrite: bool) -> None:
+        overwrite_map = {True: "w", False: "a"}
+        with open(self.filepath, mode=overwrite_map[overwrite], newline="") as f:
+            fieldnames = [adm.name for adm in fields(AnimeDataModel)]
+            writer = csv.DictWriter(f, fieldnames=fieldnames)
+            if self._is_empty_file():
+                writer.writeheader()
+            writer.writerow(asdict(data))
 
-    def _read_file(self) -> pd.DataFrame:
-        try:
-            return pd.read_csv(self.filepath)
-        except pd.errors.EmptyDataError:
-            return self._get_empty_dataframe_model(AnimeDataModel)
-        except FileNotFoundError:
-            self._get_empty_dataframe_model(AnimeDataModel).to_csv(
-                self.filepath, index=False
-            )
-            return pd.read_csv(self.filepath)
-
-    def _get_empty_dataframe_model(self, model: type[AnimeDataModel]) -> pd.DataFrame:
-        return pd.DataFrame(columns=[f.name for f in fields(model)])
+    def _is_empty_file(self) -> bool:
+        return self.filepath.stat().st_size == 0
